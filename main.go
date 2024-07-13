8c2bf89612bf3f3d68063411768a333c71eed2d6
Vinícius Yuji <111237473+viniyuji@users.noreply.github.com>
2024-07-13 14:49:44 -0300

    Add files via upload

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"math/rand"
 )
 
+// Array Generators
+
 func generateSortedArray(size int) []int {
 	array := make([]int, size)
 	for i := 0; i < size; i++ {
@@ -44,6 +46,8 @@ func generateAlmostSortedArray(size int) []int {
 	return array
 }
 
+// Linear Search
+
 func linearSearch(array []int, element int) int {
 	for index, value := range array {
 		if value == element {
@@ -52,6 +56,8 @@ func linearSearch(array []int, element int) int {
 	}
 	return -1
 }
+
+// Binary Search
 
 func binarySearch(array []int, element int) int {
 	left := 0
@@ -71,6 +77,8 @@ func binarySearch(array []int, element int) int {
 
 	return -1
 }
+
+// Binary Search Tree
 
 type Node struct {
 	value int
@@ -245,9 +253,193 @@ func AVLTreeFromArray(array []int) *AVLTree {
 	return &avl
 }
 
+// Red-Black Tree
+
+type redBlackNode struct {
+	value int
+	color int //0 - black, 1 - red
+	left  *redBlackNode
+	right *redBlackNode
+}
+
+type redBlackTree struct {
+	root *redBlackNode
+}
+
+func (tree *redBlackTree) isBlack(node *redBlackNode) int {
+	if node == nil || node.color == 0 {
+		return 1
+	}
+	return 0
+}
+
+func (tree *redBlackTree) isRed(node *redBlackNode) int {
+	if node != nil && node.color == 1 {
+		return 1
+	}
+	return 0
+}
+
+// func (tree *redBlackTree) blackHeight(node *redBlackNode) int {
+// 	if node == nil {
+// 		return 0
+// 	}
+
+// 	left_height := tree.blackHeight(node.left)
+// 	right_height := tree.blackHeight(node.right)
+
+// 	if left_height > right_height {
+// 		return left_height + tree.isBlack(node)
+// 	} else {
+// 		return right_height + tree.isBlack(node)
+// 	}
+// }
+
+func (tree *redBlackTree) changeColor(node *redBlackNode) {
+	node.color = 1 - node.color
+	node.left.color = 1 - node.left.color
+	node.right.color = 1 - node.right.color
+}
+
+func (tree *redBlackTree) leftRotate(node *redBlackNode) {
+	new_root := node.right
+	node.right = new_root.left
+	new_root.left = node
+
+	if tree.root == node {
+		tree.root = new_root
+	}
+}
+
+func (tree *redBlackTree) rightRotate(node *redBlackNode) {
+	new_root := node.left
+	node.left = new_root.right
+	new_root.right = node
+	if tree.root == node {
+		tree.root = new_root
+	}
+}
+
+func (tree *redBlackTree) leftBalance(nodeParent *redBlackNode, nodeSon *redBlackNode, nodeGrandParent *redBlackNode) {
+	if tree.isRed(nodeGrandParent.right) == 1 {
+		tree.changeColor(nodeGrandParent)
+		return
+	} else {
+		if nodeSon == nodeParent.right {
+			tree.leftRotate(nodeParent)
+			return
+		}
+		tree.changeColor(nodeParent)
+		tree.changeColor(nodeGrandParent)
+		tree.rightRotate(nodeGrandParent)
+		return
+	}
+}
+
+func (tree *redBlackTree) rightBalance(nodeParent *redBlackNode, nodeSon *redBlackNode, nodeGrandParent *redBlackNode) {
+	if tree.isRed(nodeGrandParent.left) == 1 {
+		tree.changeColor(nodeGrandParent)
+		return
+	} else {
+		if nodeSon == nodeParent.left {
+			tree.rightRotate(nodeParent)
+			return
+		}
+		tree.changeColor(nodeParent)
+		tree.changeColor(nodeGrandParent)
+		tree.leftRotate(nodeGrandParent)
+		return
+	}
+}
+
+func (tree *redBlackTree) nodeBalance(nodeParent *redBlackNode, nodeSon *redBlackNode, nodeGrandParent *redBlackNode) {
+	if nodeGrandParent != nil && tree.isRed(nodeParent) == 1 && tree.isRed(nodeSon) == 1 {
+		if nodeParent == nodeGrandParent.left {
+			tree.leftBalance(nodeParent, nodeSon, nodeGrandParent)
+		} else {
+			tree.rightBalance(nodeParent, nodeSon, nodeGrandParent)
+		}
+	}
+}
+
+func (tree *redBlackTree) insertNode(root *redBlackNode, parent *redBlackNode, value int) {
+	if root == nil {
+		root = &redBlackNode{value: value, color: 1}
+		// new_node := &redBlackNode{value: value, color: 1}
+		// if parent == nil {
+		// 	tree.root = new_node
+		// } else if value < parent.value {
+		// 	parent.left = new_node
+		// } else {
+		// 	parent.right = new_node
+		// }
+
+	} else if value < root.value {
+		tree.insertNode(root.left, root, value)
+		tree.nodeBalance(root, root.left, parent)
+	} else if value > root.value {
+		tree.insertNode(root.right, root, value)
+		tree.nodeBalance(root, root.right, parent)
+	}
+}
+
+func (tree *redBlackTree) insert(value int) {
+	tree.insertNode(tree.root, nil, value)
+	// tree.root.color = 0
+
+	if tree.root == nil {
+		fmt.Println("Root is nil")
+		return
+	} else {
+		tree.root.color = 0
+	}
+}
+
+func (tree *redBlackTree) searchNode(root *redBlackNode, value int) bool {
+	if root == nil {
+		return false
+	}
+	if root.value == value {
+		return true
+	}
+
+	if value < root.value {
+		return tree.searchNode(root.left, value)
+	} else {
+		return tree.searchNode(root.right, value)
+	}
+}
+
+func (tree *redBlackTree) search(value int) bool {
+	return tree.searchNode(tree.root, value)
+}
+
+func redBlackTreeFromArray(array []int) *redBlackTree {
+	tree := redBlackTree{}
+
+	for _, value := range array {
+		tree.insert(value)
+	}
+
+	return &tree
+}
+
 func main() {
-	array := generateSortedArray(100)
-	x := AVLTreeFromArray(array)
-	fmt.Println(array)
-	fmt.Println(x.search(101))
+	array_generators := []func(int) []int{generateSortedArray, generateReversedSortedArray, generateRandomSortedArray, generateAlmostSortedArray}
+	//sizes := []int{10, 100, 1000, 10000, 100000, 1000000}
+
+	for _, array_generator := range array_generators {
+		array := array_generator(5)
+		tree := redBlackTreeFromArray(array)
+		fmt.Println("================================================")
+		for true {
+			if tree.root == nil {
+				break
+			}
+
+			x := tree.root.right
+			fmt.Println(x.value)
+			x = x.right
+		}
+	}
 }
